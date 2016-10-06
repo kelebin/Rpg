527a7c4a2c352cc31a04c01c6fce8e806e5d5d08
Kevin Andrade <kevinandrade_10@hotmail.com>
2016-10-05 21:30:02 -0300

    Add files via upload

---
+++ rpg.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+void rpg(){
+int op, dano,vida1,vida2;
+
+system("cls");
+printf("batalha\n");
+printf("selecione sua acao\n1-atacar\n2-usar item\n3-fugir");
+scanf("%d", &op);
+
+
+
+switch(op){
+case 1:{
+	break;
+}
+
+
+}
+
+
+
+
+
+
+}
+
+
+int main(){
+rpg();
+return 0;
+}
