0bd1b08156cfe9210ac8ae4f8d45e587f0bce4f1
Kevin Andrade <kevinandrade_10@hotmail.com>
2016-10-06 21:09:23 -0300

    Update rpg.c

--- rpg.c
+++ rpg.c
@@ -7,6 +7,11 @@ void batalha();
 
 char nome[10];
 
+typedef struct inventario{
+int pot;
+
+}inv;
+
 typedef struct personagem{
 int vida;
 int classe;
@@ -16,11 +21,11 @@ int dano,eva,def;
 
 typedef struct morcego{
 int vida;
-int dano;
+int danoini;
 }mo;
 
 typedef struct esqueleto{
-int vida,dano,eva,def;
+int vida,danoini2,eva,def;
 }es;
 
 
@@ -34,8 +39,11 @@ void menu(){
 	system("cls");
 	
 	int op,esc;
-	printf("JANIO'S ADVENTURE\n Pressione 1 para comecar: ");
+	printf("JANIO'S BIZARRE ADVENTURE\n Pressione 1 para comecar: \n Pressione 2 para sair\n");
 	scanf("%d",&esc);
+	if(esc==2){
+	exit(0);
+	}
 	system("cls");
 	printf("Selecione uma opcao\n 1-Novo jogo\n 2-Carregar jogo salvo\n 3-Sair\n");
 	scanf("%d", &op);
@@ -55,8 +63,8 @@ void menu(){
 		}
 		case 3:{
 			
-			//menu();
-			exit(0);
+			menu();
+			//exit(0);
 			break;
 		}
 	}
@@ -156,22 +164,28 @@ default:{
 }
 }
 
-void batalha(){
-int op;
-int x;
-
+void batalha(pe * x){
+int op,opinv;
+int c,ata;
+inv invent;
+invent.pot = 10;
+es esq;
 system("cls");
 
- x=1+(rand() %2);
- if (x==1){
+ c=1+(rand() %2);
+ if (c==1){
     printf("Voce encontrou um morcego!\n");
+     
  }
- if (x==2){
+ if (c==2){
     printf("voce encontrou um esqueleto!\n");
+ 	esq.vida =100;
+    //esq.dano =10;
  }
+ 
 while (op!=3){
 
-printf("selecione sua acao\n1-atacar\n2-usar item\n3-fugir\n");
+printf("selecione sua acao\n1-atacar\n2-usar item\n3-fugir\n4-Ver status do inimigo\n");
 scanf("%d", &op);
 
 
@@ -180,10 +194,32 @@ switch(op){
 case 1:{
 	system("cls");
 	printf("voce atacou\n");
+//	printf("seu dano : %d\n",x->dano);
+	ata = 0;
+	//ata = x->dano;
+	ata = 30;
+	printf("voce hitou : %d\n",ata);
+	esq.vida-=ata;
+	printf("vida do inimigo : %d\n",esq.vida);
+	if (esq.vida<=0){
+	printf("monstro derrotado\n");
+	
+	}
 	system("pause");
 	break;
 }
 case 2:{
+	printf("selecione um item pra usar :\n1-Usar potion : '%d'\n",invent.pot);
+	scanf("%d",&opinv);
+	if (invent.pot>0){
+	invent.pot-=1;
+	system("cls");
+	printf("voce  usou uma potion\n");
+	}
+	else{
+	system("cls");
+	printf("voce nao tem mais potions\n");
+	}
 	break;
 }
 case 3:{
@@ -191,7 +227,10 @@ case 3:{
 	system("pause");
 	break;
 }
-default:{
+case 4:{
+	system("cls");
+	printf("Inimigo : Esqueleto\nVida : %d\n",esq.vida);
+	system("pause");
 	break;
 }
 }
